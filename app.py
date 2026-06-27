2d6cbf685e1f3442aea532951604bc5d6b3ff198
Maxranderson Araújo <max_rodrigues_araujo@hotmail.com>
2026-06-27 21:28:31 +0000

    Add base files

---
+++ app.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import json
+import streamlit as st
+import pandas as pd
+# import numpy as np
+from urllib import request 
+
+st.set_page_config(layout="wide")
+st.title('Dashboard de atendimentos')
